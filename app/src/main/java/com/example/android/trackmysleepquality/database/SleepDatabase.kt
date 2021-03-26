28400bcba60d1eeb52d629c48b0da042d6fbadd2
최훈엽 <>
2021-03-26 17:14:04 +0900

    room데이타베이스 세팅 다음진행 <https://developer.android.com/codelabs/kotlin-android-training-coroutines-and-room#3>

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,45 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    //데이터베이스는 DAO에 대해 참조하고있어야함
+    //여러 DAO를 가질수있음
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    /*
+    이 추상 값 아래에 컴패니언 객체를 정의합니다.
+    동반 객체를 사용하면 클라이언트가 클래스를 인스턴스화하지 않고도 데이터베이스를 만들거나 가져 오는 메서드에 액세스 할 수 있습니다.
+    이 클래스의 유일한 목적은 데이터베이스를 제공하는 것이므로 인스턴스화 할 이유가 없습니다.
+     */
+    companion object {
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase? {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database")
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+
+    }
+
+
+}
